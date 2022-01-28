76136fa73875b75836d498861db5d1034c5d4fd5
UltraXPro <73195390+UltraXPro@users.noreply.github.com>
2022-01-28 07:26:35 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -26,9 +26,9 @@ async def media_receive_handler(_, m: Message):
         file_name = file.file_name
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     stream_link = Var.URL + str(log_msg.message_id) + '/' +quote_plus(file_name) if file_name else ''
-    stream_links = f"https://1.url2go.in/intresting"
+    stream_links = f"https://telegram.me/StreamTVIndex"
     await m.reply_text(
-        text=f"https://stream.shorturllink.in/st?api=af5e38dfaf8b900b45335173d279b44d7ae4b2e9&url={stream_link}",
+        text=f"{stream_link}",
         quote=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Learn something interesting 🤔', url=stream_links)]])
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⭕ Join Our Channel ⭕', url=stream_links)]])
     )
